a2880a392abbc82ee08c47d1c53e8fcf11e40a02
pastoreli <pastoreli.igor@yahoo.com>
2019-05-04 21:27:12 -0300

    add: web api route

--- API/axios.js
+++ API/axios.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const route = 'http://localhost:8080';
+// const route = 'http://localhost:8080';
+const webRoute = 'https://poke-tonhao.herokuapp.com';
 
 export default axios.create({
-  baseURL: route,
+  baseURL: webRoute,
   timeout: 100000000,
 })
