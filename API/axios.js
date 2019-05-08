afbffc53b810229bc4b8577384db904f7991ea84
nicolas010256 <nicolas010256@gmail.com>
2019-05-08 00:07:37 -0300

    fix: axios

--- API/axios.js
+++ API/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // const route = 'http://localhost:8080';
-//const webRoute = 'https://poke-tonhao.herokuapp.com';
-const webRoute = 'http://localhost:8080';
+const webRoute = 'https://poke-tonhao.herokuapp.com';
+//const webRoute = 'http://localhost:8080';
 
 export default axios.create({
   baseURL: webRoute,
