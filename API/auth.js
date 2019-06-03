37bd1f7236b990960d65d6d63e8722578daed03f
nicolas <nicola010256@gmail.com>
2019-06-03 15:32:07 -0300

    fix: auth

--- API/auth.js
+++ API/auth.js
@@ -4,7 +4,11 @@ const route = '/auth';
 export default {
   signIn: async (data) => {
     console.log(JSON.stringify(data))
-    return axios.create().post(route, JSON.stringify(data))
+    return axios.create().post(route, JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
       .then( res => {
       // console.log('login', res)
       return res.headers['authorization']
