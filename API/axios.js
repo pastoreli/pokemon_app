346d567f25d362b1c6d2507076c6fbf94fff016e
pastoreli <pastoreli.igor@yahoo.com>
2019-06-19 21:13:20 -0300

    fix

--- API/axios.js
+++ API/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // const route = 'http://localhost:8080';
 // const route = 'http://localhost:8000';
-const route = 'https://poke-tonhao.herokuapp.com';
+const route = 'https://poke-api-fatec.herokuapp.com';
 
 const checkIsExistToken = () => {
   const accessToken = localStorage.getItem('accessToken')
